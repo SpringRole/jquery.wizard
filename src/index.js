d8c027246847ada9b80ee433b1a3029200f6b719
gmjacob <george.mammenj@gmail.com>
2018-07-22 20:30:30 +0530

    tick image and css issues need to be fixed, main function is working well

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var Templates = require('./templates')
+import Templates from './templates.js';
 
 /**
  * Usage
@@ -22,50 +22,88 @@ var Templates = require('./templates')
 
     $.fn.wizard = function ( options ) {
         
+        // var Templates = {
+        //     "default" : {
+        //         "classes" : {
+        //                 "current":'wizard-stage-number-active',
+        //                 "complete":"wizard-stage-number-checked",
+        //             },
+        //             "special_elements" : {
+        //                 "complete":'<div class="wizard-check-icon">'+
+        //                             '<img src="/images/tick-wizard-icon.png"/>'+
+        //                             '</div>'
+        //             },
+        //             "elements" : {
+        //                 'main-section':'<div class="wizard wizard-body-selector"></div>',
+        //                 'content':'<div class="wizard-child">'+
+        //                                     '<div class="wizard-child-inner">'+
+        //                                         '<div class="wizard-stage">'+
+        //                                             '<div class="wizard-stage-number wizard-step-section-selector">'+
+        //                                                 "<h6 class='wizard-step-selector'>1</h6>"+
+        //                                             '</div>'+
+        //                                         '</div>'+
+        //                                     '</div>'+
+        //                                     '<div class="wizard-stage-txt wizard-label-section-selector">'+
+        //                                         "<p class='wizard-label-selector'>Placeholder</p>"+
+        //                                     '</div>'+
+        //                                     '<div class="wizard-stage-line wizard-line-selector"></div>'+
+        //                                 '</div>'
+        //             }
+        //     }
+        // }
+
         var settings = $.extend({
             current_page:'1',
             wizardName:'default',
             pages_with_labels:{'1':'Step One', '2':'Step Two', '3':'Step Three'},
+            template:'default'
         }, options );
-                
-        var $currentElement = $(this)
-        var reachedCurrentPage = false
-        var currentSection = "sectionComplete"
-        $currentElement.html(elements['main-section'])
+
+        var template = Templates[settings.template];
+        var $parentElement = $(this);
+        var reachedCurrentPage = false;
+        var currentSection;
+
+        $parentElement.html(template["elements"]['main-section'])
         
         for(var page in settings.pages_with_labels){
-            var step_class='';
-            var $completedElement = $(elements['content'])
+            //The completed element
+            var $element = $(template["elements"]['content'])
+            
             var pages_array = Object.keys(settings.pages_with_labels)
             var lastPage = pages_array[pages_array.length-1]
             
-            //Check to see if the current page has been reached
+            //Check to see if the current page has been reached, This check happens until the current page has reached
             if(!reachedCurrentPage){
                 reachedCurrentPage = (page == settings.current_page ? true : false)
-                currentSection = reachedCurrentPage ? "currentSection" : "sectionComplete"
-            }
-            //If the current page has not been reached then select the class for complete and the required labels
-            if(!reachedCurrentPage){
-                step_class = 'complete';
-                $completedElement.find('.wizard-step-section-selector')
-                                                .html(special_elements['completedLabel'])
-            }
-            else {   //If its the current page  reached then select the class for the current page
-                page == settings.current_page ? step_class = 'current' :  null
-                $completedElement.find('.wizard-step-selector').html(page)
             }
             
+            //If the current page has not been reached then the current section is of type complete
+            if(!reachedCurrentPage){
+                currentSection = 'complete'
+            }
+            else {
+                currentSection = (page == settings.current_page)? "current" : "incomplete"
+            }
+            // console.log('page--',page);
+            // console.log('element ',$element.find('.wizard-step-selector'))
+            if(template["special_elements"][currentSection]){
+                $element.find('.wizard-step-section-selector').html(template["special_elements"][currentSection])
+            }  else { 
+                $element.find('.wizard-step-selector').html(page)
+            }
+            console.log('truth test', template["classes"][currentSection])
+            if(template["classes"][currentSection]){
+                $element.find('.wizard-step-section-selector').addClass(template["classes"][currentSection])
+                }
+
+            $element.find('.wizard-label-selector').html(settings.pages_with_labels[page])
+
             if (page === lastPage)
-                $completedElement.find('.wizard-line-selector')
-                                                .remove()
-                                    
-            $completedElement.find('.wizard-step-section-selector')
-                                            .addClass(classes[step_class])
-            $completedElement.find('.wizard-label-selector')
-                                            .html(settings.pages_with_labels[page])
+                $element.find('.wizard-line-selector').remove()
             
             //If its the first insertion it needs to be the inner html of the body, else be inserted after the last wizard element class
-            $currentElement.find('.wizard-child').length > 0 ? $completedElement.insertAfter($currentElement.find('.wizard-child:last')) : $currentElement.html($completedElement)
+            $parentElement.find('.wizard-child').length > 0 ? $element.insertAfter($parentElement.find('.wizard-child:last')) : $parentElement.html($element)
                 
         }
         return this
